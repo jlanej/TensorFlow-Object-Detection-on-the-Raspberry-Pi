51a8f3e40868f788117e990dc6c88c6226024b67
jlanej <jlanej@gmail.com>
2019-10-26 11:25:30 -0500

    update path

--- Pet_detector.get.py
+++ Pet_detector.get.py
@@ -1,3 +1,4 @@
 
 cd /home/pi/tensorflow1/models/research/object_detection
 wget https://raw.githubusercontent.com/jlanej/TensorFlow-Object-Detection-on-the-Raspberry-Pi/master/Pet_detector.py
+python3 Pet_detector.py.1
